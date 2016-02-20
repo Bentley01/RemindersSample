54c4a6fa499950ffe977a74bbfe2c3faac10d35c
Paul Hewitt <paul@paulshewittca.ca>
2016-02-20 15:51:05 -0500

    Adds new reminder and exit menu options

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -55,11 +56,19 @@ public class RemindersActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        switch (id) {
 
-        return super.onOptionsItemSelected(item);
+            case R.id.action_new:
+                // create a new reminder
+                Log.d(getLocalClassName(), "create new Reminder");
+                return true;
+
+            case R.id.action_exit:
+                // exit program
+                finish();
+                return true;
+            default:
+                return false;
+        }
     }
 }
